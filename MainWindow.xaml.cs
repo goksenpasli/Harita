c0e6ec6ade13fdb27d73b8eadd982a6459b40b6f
goksenpasli <gpasli@live.com>
2019-02-18 20:14:16 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Linq;
+namespace Harita
+{
+
+    public partial class MainWindow: Window
+	{
+        public MainWindow() => InitializeComponent();
+        void Document_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) => AddHandler(MouseLeftButtonUpEvent, new RoutedEventHandler(Tıkla));
+        void Tıkla(object x, RoutedEventArgs e)
+		{
+            var veri = new Veri();
+            if (!(e.OriginalSource is Path))
+            {
+                MessageBox.Show("İllerin Üzerine Tıklayın.");
+                return;
+            }
+            if (e.OriginalSource is Path p && tbilismi.Text == p.Name)
+            {
+                MessageBox.Show("Tebrikler Bildin.");
+                p.Visibility = Visibility.Collapsed;
+                veri.iller.Remove(p.Name);
+                if (veri.iller.Any()) Veri.Yeniİl(this);
+                else
+                    MessageBox.Show("Bitti.");
+            }
+            else
+                MessageBox.Show("Cevap Yanlış.");
+            e.Handled = true;
+		}
+        void Window_Loaded(object sender, RoutedEventArgs e) => Veri.Yeniİl(this);
+    }
+}
---
+++ İller.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+namespace Harita
+{
+	public  class Veri
+	{
+        public List<string> iller;
+
+        public Veri() => iller = new List<string>
+            {
+                "Adana",
+                "Adıyaman",
+                "Afyonkarahisar",
+                "Ağrı",
+                "Amasya",
+                "Ankara",
+                "Antalya",
+                "Artvin",
+                "Aydın",
+                "Balıkesir",
+                "Bilecik",
+                "Bingöl",
+                "Bitlis",
+                "Bolu",
+                "Burdur",
+                "Bursa",
+                "Çanakkale",
+                "Çankırı",
+                "Çorum",
+                "Denizli",
+                "Diyarbakır",
+                "Edirne",
+                "Elazığ",
+                "Erzincan",
+                "Erzurum",
+                "Eskişehir",
+                "Gaziantep",
+                "Giresun",
+                "Gümüşhane",
+                "Hakkâri",
+                "Hatay",
+                "Isparta",
+                "İçel",
+                "İstanbul",
+                "İzmir",
+                "Kars",
+                "Kastamonu",
+                "Kayseri",
+                "Kırklareli",
+                "Kırşehir",
+                "Kocaeli",
+                "Konya",
+                "Kütahya",
+                "Malatya",
+                "Manisa",
+                "Kahramanmaraş",
+                "Mardin",
+                "Muğla",
+                "Muş",
+                "Nevşehir",
+                "Niğde",
+                "Ordu",
+                "Rize",
+                "Sakarya",
+                "Samsun",
+                "Siirt",
+                "Sinop",
+                "Sivas",
+                "Tekirdağ",
+                "Tokat",
+                "Trabzon",
+                "Tunceli",
+                "Şanlıurfa",
+                "Uşak",
+                "Van",
+                "Yozgat",
+                "Zonguldak",
+                "Aksaray",
+                "Bayburt",
+                "Karaman",
+                "Kırıkkale",
+                "Batman",
+                "Şırnak",
+                "Bartın",
+                "Ardahan",
+                "Iğdır",
+                "Yalova",
+                "Karabük",
+                "Kilis",
+                "Osmaniye",
+                "Düzce"};
+
+        public static string İl
+		{
+			get;
+			set;
+		}
+		static readonly Random rd = new Random();
+        public static void Yeniİl(MainWindow mv) => mv.tbilismi.Text = new Veri().iller[rd.Next(0, new Veri().iller.Count)];
+    }
+}
